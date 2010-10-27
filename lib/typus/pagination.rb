18c4b5a51fbf48fb91082e4042d471879ef1d4ed
guns <sung@metablu.com>
2010-10-27 01:45:08 -0500

    include page numbers in will_paginate block

--- lib/typus/pagination.rb
+++ lib/typus/pagination.rb
@@ -3,13 +3,14 @@ module Typus
   class Pagination < WillPaginate::ViewHelpers::LinkRenderer
 
     def to_html
-      previous_label = "&larr; " + _t("Previous")
-      next_label = _t("Next") + " &rarr;"
+      html = pagination.map do |item|
+        (item.is_a? Fixnum) ? (page_number item) : (send item)
+      end
 
-      html = [previous_or_next_page(@collection.previous_page, previous_label, 'left'),
-              previous_or_next_page(@collection.next_page, next_label, 'right')]
+      html[0]  = previous_or_next_page @collection.previous_page, ("&larr; " + _t("Previous")), 'left'
+      html[-1] = previous_or_next_page @collection.next_page, (_t("Next") + " &rarr;"), 'right'
 
-      html_container(html)
+      html_container html.join(@options[:separator])
     end
 
   end
